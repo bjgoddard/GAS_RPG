880706c2e6925195cecc9654c5e476a1f5e5ab95
Ben Goddard <56774222+bjgoddard@users.noreply.github.com>
2023-10-09 13:46:32 -0700

    Initial commit
    Testing 5.3, Rider, new cpp class

---
+++ Aura/Intermediate/Build/Win64/x64/AuraEditor/Development/Core/SharedPCH.Core.Cpp20.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.37.32824
+#include "C:/Users/muzik/Documents/GitHub/GAS_RPG/Aura/Intermediate/Build/Win64/x64/AuraEditor/Development/Core/SharedPCH.Core.Cpp20.h"
---
+++ Aura/Intermediate/Build/Win64/x64/AuraEditor/Development/UnrealEd/SharedPCH.UnrealEd.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
+#include "C:/Users/muzik/Documents/GitHub/GAS_RPG/Aura/Intermediate/Build/Win64/x64/AuraEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
+#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
